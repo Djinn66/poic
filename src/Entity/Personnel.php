f40d289f20f6692d6a2b35044bee24359fef7cf2
unknown <laurent.cuadras@gmail.com>
2021-01-19 14:47:12 +0100

    initial commit

---
+++ src/Entity/Personnel.php
@@ -0,0 +1,194 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PersonnelRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=PersonnelRepository::class)
+ */
+class Personnel
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $prenom;
+
+    /**
+     * @ORM\Column(type="string", length=1)
+     */
+    private $sexe;
+
+    /**
+     * @ORM\Column(type="string", length=10)
+     */
+    private $nia;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nni;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nid;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nsap;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $date_de_naissance;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $lieu_naissance;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $email;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(string $prenom): self
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    public function getSexe(): ?string
+    {
+        return $this->sexe;
+    }
+
+    public function setSexe(string $sexe): self
+    {
+        $this->sexe = $sexe;
+
+        return $this;
+    }
+
+    public function getNia(): ?string
+    {
+        return $this->nia;
+    }
+
+    public function setNia(string $nia): self
+    {
+        $this->nia = $nia;
+
+        return $this;
+    }
+
+    public function getNni(): ?string
+    {
+        return $this->nni;
+    }
+
+    public function setNni(string $nni): self
+    {
+        $this->nni = $nni;
+
+        return $this;
+    }
+
+    public function getNid(): ?string
+    {
+        return $this->nid;
+    }
+
+    public function setNid(string $nid): self
+    {
+        $this->nid = $nid;
+
+        return $this;
+    }
+
+    public function getNsap(): ?string
+    {
+        return $this->nsap;
+    }
+
+    public function setNsap(string $nsap): self
+    {
+        $this->nsap = $nsap;
+
+        return $this;
+    }
+
+    public function getDateDeNaissance(): ?\DateTimeInterface
+    {
+        return $this->date_de_naissance;
+    }
+
+    public function setDateDeNaissance(\DateTimeInterface $date_de_naissance): self
+    {
+        $this->date_de_naissance = $date_de_naissance;
+
+        return $this;
+    }
+
+    public function getLieuNaissance(): ?string
+    {
+        return $this->lieu_naissance;
+    }
+
+    public function setLieuNaissance(string $lieu_naissance): self
+    {
+        $this->lieu_naissance = $lieu_naissance;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+}
---
+++ src/Form/PersonnelType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Personnel;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PersonnelType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nom')
+            ->add('prenom')
+            ->add('sexe')
+            ->add('nia')
+            ->add('nni')
+            ->add('nid')
+            ->add('nsap')
+            ->add('date_de_naissance')
+            ->add('lieu_naissance')
+            ->add('email')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Personnel::class,
+        ]);
+    }
+}
